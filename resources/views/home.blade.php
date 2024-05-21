c915d64f31aac4baa3708c4c04d85d2ee34f0fa5
shahjahan45 <sjeel663@gmail.com>
2024-05-21 13:12:38 +0300

    template rendering

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+@section('content')
+    
+
+
+<main role="main" class="container">
+    <h1 class="mt-5 text-danger">Home</h1>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ratione quaerat vero a, ullam reiciendis earum distinctio nihil exercitationem quidem neque odit aliquid quasi esse, repudiandae, adipisci non placeat.
+  </main>
+  @endsection
